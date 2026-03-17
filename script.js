cda6b765bab4abda7536f1e718bb2ab37e21c8dc
V4Eren <gumpseven64@gmail.com>
2026-03-18 01:43:40 +0300

    Numbers can now be added to the display

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const numberButtons = document.querySelectorAll('.num-button');
+const display = document.querySelector('.displayed-value')
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -29,3 +32,12 @@ function operation(num1, num2, operator) {
     }
 }
 
+function appendNumber(num) {
+    display.textContent += num;
+}
+
+
+
+numberButtons.forEach((num) => {
+    num.addEventListener('click', () => appendNumber(num.textContent))
+})
